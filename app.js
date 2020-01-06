c371bb4def3093124f5667bccecc4215dbc72268
jaime <jaime.rodrigo28@gmail.com>
2020-01-05 21:10:54 -0800

    Middleware for history api fallback

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var history = require('connect-history-api-fallback');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,14 +15,33 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(history({
+    rewrites: [
+      {
+        from: /^\/api\/.*$/,
+        to: function(context) {
+            return context.parsedUrl.path
+        }
+      },
+      {
+        from: /^\/images\/.*$/,
+        to: function(context) {
+            return context.parsedUrl.path
+        }
+      }
 
-app.use('/', indexRouter);
+    ]
+ }))
+
+//app.use('/', indexRouter);
+app.use('/',indexRouter)
 app.use('/users', usersRouter);
 app.use('/images', imagesRouter);
 
